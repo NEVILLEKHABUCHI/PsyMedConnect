a5a7e30045c366c7249fb9dc6644d2c4f84d353a
Neville Khabuchi <nevillekhabuchi@gmail.com>
2025-03-01 09:00:40 +0300

    Adding the nav bars on the about, contact and submit details page

--- controllers/clientsControllers.js
+++ controllers/clientsControllers.js
@@ -1,3 +1,15 @@
 exports.getHome = (req, res) => {
     res.render('home', {title: 'Home'});
 }
+
+exports.getAbout = (req, res) => {
+    res.render('about', {title: 'About'});
+}
+
+exports.getContacts = (req, res) => {
+    res.render('contact', {title: 'Contact Us'});
+}
+
+exports.getSubmitDetails = (req, res) => {
+    res.render('submitDetails', {title: 'Submit Details'});
+}
--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const clientsController = require('../controllers/clientsControllers');
 
 router.get('/', clientsController.getHome);
+router.get('/about', clientsController.getAbout);
+router.get('/contact', clientsController.getContacts);
+router.get('/submitDetails', clientsController.getSubmitDetails);
 
 module.exports = router;
